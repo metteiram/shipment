d34c969bbd6df7baaa17875baeb5481d17d1d4e6
Maryia Martsul <martsul.mary@gmail.com>
2023-05-13 14:00:13 +0300

    update views

--- shipments/views.py
+++ shipments/views.py
@@ -1,11 +1,40 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 from .models import Shipment
 from .serializers import ShipmentSerializer
 
 class ShipmentList(generics.ListCreateAPIView):
     queryset = Shipment.objects.all()
     serializer_class = ShipmentSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ShipmentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Shipment.objects.all()
     serializer_class = ShipmentSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
